447004f6df6b972c6bdfea1f789584e8d14eb73c
coldcatzzz <acat1212@163.com>
2021-07-28 19:00:45 +0800

    modifier

---
+++ com/test/modifier2/Test.java
@@ -0,0 +1,11 @@
+package com.test.modifier2;
+
+import com.test.modifier.A;
+
+public class Test {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.n1);
+        a.m1();
+    }
+}
